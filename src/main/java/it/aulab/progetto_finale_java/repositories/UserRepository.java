81161a8d69395c59183904268dc6a7a13a5466a0
MarinoLavopa <marinolavopa@hotmail.it>
2026-05-04 18:14:29 +0200

    primo commit

---
+++ src/main/java/it/aulab/progetto_finale_java/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package it.aulab.progetto_finale_java.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import it.aulab.progetto_finale_java.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository < User , Long> {
+    User findByEmail(String email);
+}
---
+++ src/main/java/it/aulab/progetto_finale_java/services/UserService.java
@@ -0,0 +1,14 @@
+package it.aulab.progetto_finale_java.services;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import it.aulab.progetto_finale_java.dtos.UserDTO;
+import it.aulab.progetto_finale_java.models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface UserService {
+
+    void saveUser(UserDTO userDTO, RedirectAttributes redirectAttributes, HttpServletRequest request, HttpServletResponse response);
+    User findUserByEmail(String email);
+}
